ebe4477a765d6b4e50a528a142f986d325a89db4
M.M.Hasibuzzaman <52198431+H-Zaman@users.noreply.github.com>
2022-09-13 23:17:04 +0600

    firebase hosting added

--- lib/src/utilities/helper/localization/_temp_en.dart
+++ lib/src/utilities/helper/localization/_temp_en.dart
@@ -1,5 +1,5 @@
 Map<String, String> enLang = {
-  'splash_header' : 'Beautiful Workspace in the heart\nof your city',
+  'splash_header' : 'qwe',
   'splash_subtitle' : 'Whether you need a desk, office, suite, or entire HQ, we create environments that increase productivity, innovation and collaboration.',
   'sign_in_workspace' : 'Sign in to workspace',
   'log_in' : 'Log In',
